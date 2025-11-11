355658f53ab2cb7f34cea3b0d2069749dcacf6a0
JK <justin@kufeldt.com>
2025-11-11 11:07:30 -0500

    Colors

--- src/main/java/me/pintoadmin/pintoPlaytime/PlaytimeCommand.java
+++ src/main/java/me/pintoadmin/pintoPlaytime/PlaytimeCommand.java
@@ -1,5 +1,6 @@
 package me.pintoadmin.pintoPlaytime;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.*;
 
 public class PlaytimeCommand implements CommandExecutor {
@@ -14,15 +15,15 @@ public class PlaytimeCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(args.length == 1){
             String playtime = plugin.getPlaytimeManager().getPlaytime(args[0]);
-            sender.sendMessage("Player " + args[0] + " has " + playtime + " playtime.");
+            sender.sendMessage(ChatColor.GOLD+"Player " + args[0] + " has " + playtime + " playtime.");
             return true;
         } else if(args.length != 2) {
-            sender.sendMessage("Usage: /playtime <player> <[time](m/h/d)>");
+            sender.sendMessage(ChatColor.RED+"Usage: /playtime <player> <[time](m/h/d)>");
             return true;
         }
         String player = args[0];
         String timeArg = args[1];
-        sender.sendMessage("Set playtime for player " + player + " to " + timeArg);
+        sender.sendMessage(ChatColor.GREEN+"Set playtime for player " + player + " to " + timeArg);
 
         plugin.getPlaytimeManager().setPlaytime(player, timeArg);
         return true;
