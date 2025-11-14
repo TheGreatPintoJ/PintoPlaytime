b5173efa66031a5ef8ec0a5b0d41d4b3b75749f2
TheGreatPintoJ <jukujunk@gmail.com>
2025-11-14 08:30:21 -0500

    Added compatibility for multiple permissions or commands per milestone

--- src/main/java/me/pintoadmin/pintoPlaytime/ConfigLoader.java
+++ src/main/java/me/pintoadmin/pintoPlaytime/ConfigLoader.java
@@ -7,7 +7,7 @@ import java.util.*;
 public class ConfigLoader {
     private final PintoPlaytime plugin;
     private final List<String> exclusions = new ArrayList<>();
-    private final List<Map<String, String>> milestones = new ArrayList<>();
+    private final List<Map<String, List<String>>> milestones = new ArrayList<>();
 
     public ConfigLoader(PintoPlaytime plugin) {
         this.plugin = plugin;
@@ -20,14 +20,14 @@ public class ConfigLoader {
         updateConfig();
     }
 
-    public List<Map<String, String>> getMilestones() {
+    public List<Map<String, List<String>>> getMilestones() {
         if (milestones.isEmpty()) {
             FileConfiguration config = plugin.getConfig();
             List<Map<?, ?>> configMilestones = config.getMapList("milestones");
             for (Map<?, ?> milestone : configMilestones) {
-                Map<String, String> milestoneMap = new HashMap<>();
+                Map<String, List<String>> milestoneMap = new HashMap<>();
                 for (Map.Entry<?, ?> entry : milestone.entrySet()) {
-                    milestoneMap.put(entry.getKey().toString(), entry.getValue().toString());
+                    milestoneMap.put(entry.getKey().toString(), entry.getValue() instanceof List<?> ? (List<String>) entry.getValue() : Arrays.asList(entry.getValue().toString()));
                 }
                 milestones.add(milestoneMap);
             }
